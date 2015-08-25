3814632157275a557a6e2768a3fcdfce6ba297fc
Hello_海生[公司] <hais1992@163.com>
2015-08-25 10:17:58 +0800

    上传 Demo 。。

--- Demo_src/src/main/java/pw/hais/utils_demo/activity/MainActivity.java
+++ Demo_src/src/main/java/pw/hais/utils_demo/activity/MainActivity.java
@@ -19,7 +19,6 @@ import pw.hais.utils_demo.R;
 import pw.hais.utils_demo.adapter.WeatherAdapter;
 import pw.hais.utils_demo.app.BaseActivity;
 import pw.hais.utils_demo.entity.Weather;
-import pw.hais.utils_demo.utils.AnWeiJiangUtils;
 
 public class MainActivity extends BaseActivity {
     //反射免FindViewById，名称必须和 XML的 控件ID一样
@@ -31,12 +30,43 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*--------  免FindViewById  -----------*/
+        Typeface iconfont = Typeface.createFromAsset(getAssets(), "fonts/iconfont.ttf");
+        text_hello.setTypeface(iconfont);
+        text_hello.setText(getResources().getString(R.string.font));
 
-        text_hello.setOnClickListener(new View.OnClickListener() {
+        /*---------      请求网络       --------*/
+        loadDialog.show("请求网络中...");
+        String url = "http://apis.baidu.com/apistore/weatherservice/weather";
+        Http.getObject(Request.Method.GET, url, null, new Listener<Weather>() {
             @Override
-            public void onClick(View view) {
-                AnWeiJiangUtils.getGameResults("hais", 5);
+            public void success(Weather response) {
+                L.i("Http Hais解决了getObject的问题："+gson.toJson(response));
+            }
+
+            @Override
+            public void httpEnd(boolean isTrue) {
+                loadDialog.dismiss();
             }
         });
+
+        /*-------- 简易Adapter  -------*/
+        List<Weather> weatherList = new ArrayList<>();
+        weatherList.add(new Weather(1,"111111"));
+        weatherList.add(new Weather(2,"222222"));
+        weatherList.add(new Weather(3,"333333"));
+        weatherList.add(new Weather(4,"444444"));
+        final WeatherAdapter weatherAdapter = new WeatherAdapter(weatherList,R.layout.activity_main_list_item, WeatherAdapter.ViewHolder.class);
+        listview.setAdapter(weatherAdapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                L.showShort("点击："+weatherAdapter.getItem(i).errMsg);
+            }
+        });
+
+
+
+
     }
 }
